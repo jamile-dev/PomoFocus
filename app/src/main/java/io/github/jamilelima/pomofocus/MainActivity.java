345fb1e0ecd3a54bf88ff30c31ecc918386f79f6
jamilelima <jamilesousalima@gmail.com>
2018-12-15 14:44:15 -0300

    Add ViewPager and TabLayout

--- app/src/main/java/io/github/jamilelima/pomofocus/MainActivity.java
+++ app/src/main/java/io/github/jamilelima/pomofocus/MainActivity.java
@@ -1,5 +1,7 @@
 package io.github.jamilelima.pomofocus;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout mytabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
