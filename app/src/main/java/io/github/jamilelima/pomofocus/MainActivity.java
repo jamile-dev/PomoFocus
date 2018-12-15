9bc0f9fc67b66d790d9cafd62cdb92f5aa7167b2
jamilelima <jamilesousalima@gmail.com>
2018-12-15 12:00:07 -0300

    Add Toolbar and styles

--- app/src/main/java/io/github/jamilelima/pomofocus/MainActivity.java
+++ app/src/main/java/io/github/jamilelima/pomofocus/MainActivity.java
@@ -2,12 +2,21 @@ package io.github.jamilelima.pomofocus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
     }
+
+
 }
