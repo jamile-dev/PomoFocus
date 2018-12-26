1047c9d301e4d8a23e43ae82f71b76898ff0ea25
jamilelima <jamilesousalima@gmail.com>
2018-12-26 10:51:13 -0300

    Add Model package - add button on NewTask.xml  and set fields on NewTaskActivity

---
+++ app/src/main/java/io/github/jamilelima/pomofocus/Model/Task.java
@@ -0,0 +1,5 @@
+package io.github.jamilelima.pomofocus.Model;
+
+public class Task {
+
+}
--- app/src/main/java/io/github/jamilelima/pomofocus/NewTaskActivity.java
+++ app/src/main/java/io/github/jamilelima/pomofocus/NewTaskActivity.java
@@ -3,10 +3,20 @@ package io.github.jamilelima.pomofocus;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewTaskActivity extends AppCompatActivity {
 
+  private static final String TAG = "NewTaskActivity";
+
     private Toolbar newTaskToolbar;
+
+    EditText taskTitle;
+    EditText taskDescription;
+    Button saveTaskButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +28,22 @@ public class NewTaskActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("New task");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        taskTitle = findViewById(R.id.task_title);
+        taskDescription = findViewById(R.id.task_description);
+        saveTaskButton = findViewById(R.id.save_task_button);
+
+        this.saveTask();
+    }
+
+    public void saveTask() {
+      saveTaskButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          // TODO: Implementing save task on database
+          Log.d(TAG, "Clicked button save task");
+        }
+      });
     }
 
 
