033b83925d932ff1b449fa263e567477fad8d668
jamilelima <jamilesousalima@gmail.com>
2018-12-26 11:17:16 -0300

    Add Task model

--- app/src/main/java/io/github/jamilelima/pomofocus/Model/Task.java
+++ app/src/main/java/io/github/jamilelima/pomofocus/Model/Task.java
@@ -1,5 +1,61 @@
 package io.github.jamilelima.pomofocus.Model;
 
+
+
 public class Task {
+
+  private int id;
+  private String title;
+  private String description;
+  private Number pomodoro_amount;
+  private boolean isCompleted;
+
+  public Task(int id, String title, String description, Number pomodoro_amount, boolean isCompleted) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.pomodoro_amount = pomodoro_amount;
+    this.isCompleted = isCompleted;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Number getPomodoro_amount() {
+    return pomodoro_amount;
+  }
+
+  public void setPomodoro_amount(Number pomodoro_amount) {
+    this.pomodoro_amount = pomodoro_amount;
+  }
+
+  public boolean isCompleted() {
+    return isCompleted;
+  }
+
+  public void setCompleted(boolean completed) {
+    isCompleted = completed;
+  }
 
 }
