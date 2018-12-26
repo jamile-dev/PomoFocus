613def5b1c4f88393ad4e5d644a48de56afcec0a
jamilelima <jamilesousalima@gmail.com>
2018-12-26 16:20:21 -0300

    Initializing layout of PomodoroTimerActivity

--- app/src/main/java/io/github/jamilelima/pomofocus/PomodoroTimerActivity.java
+++ app/src/main/java/io/github/jamilelima/pomofocus/PomodoroTimerActivity.java
@@ -2,12 +2,22 @@ package io.github.jamilelima.pomofocus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class PomodoroTimerActivity extends AppCompatActivity {
+
+  private Toolbar mToolbar;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_pomodoro_timer);
+
+    mToolbar = findViewById(R.id.main_page_timer_toolbar);
+    setSupportActionBar(mToolbar);
+    getSupportActionBar().setTitle("Pomo Timer");
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    getSupportActionBar().setDisplayShowHomeEnabled(true);
   }
 }
