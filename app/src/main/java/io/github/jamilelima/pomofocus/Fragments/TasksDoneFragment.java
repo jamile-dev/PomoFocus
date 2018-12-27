c661111d0c34830e9eaa08298479d4f4991b99e5
jamilelima <jamilesousalima@gmail.com>
2018-12-27 14:53:50 -0300

    Create TasksDoneAdapter and integrating on TasksDoneFragment

--- app/src/main/java/io/github/jamilelima/pomofocus/Fragments/TasksDoneFragment.java
+++ app/src/main/java/io/github/jamilelima/pomofocus/Fragments/TasksDoneFragment.java
@@ -1,15 +1,26 @@
 package io.github.jamilelima.pomofocus.Fragments;
 
 
+import android.arch.persistence.room.Room;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import io.github.jamilelima.pomofocus.Adapters.TasksDoneAdapter;
+import io.github.jamilelima.pomofocus.Adapters.TasksPendingAdapter;
+import io.github.jamilelima.pomofocus.AppDatabase;
+import io.github.jamilelima.pomofocus.Model.Task;
 import io.github.jamilelima.pomofocus.R;
+import java.util.List;
 
 
 public class TasksDoneFragment extends Fragment {
+
+    private RecyclerView tasksDoneRecyclerView;
+    private TasksDoneAdapter tasksDoneAdapter;
 
 
     public TasksDoneFragment() {
@@ -21,7 +32,19 @@ public class TasksDoneFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tasks_done, container, false);
+       View rootView = inflater.inflate(R.layout.fragment_tasks_done, container, false);
+
+        AppDatabase db = Room
+            .databaseBuilder(getActivity().getApplicationContext(), AppDatabase.class, "production").allowMainThreadQueries().build();
+
+        List<Task> tasksList = db.taskDao().getDoneTasks();
+
+        tasksDoneRecyclerView = rootView.findViewById(R.id.task_done_recyclerview);
+        tasksDoneAdapter = new TasksDoneAdapter(getActivity(), tasksList);
+        tasksDoneRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        tasksDoneRecyclerView.setAdapter(tasksDoneAdapter);
+
+       return rootView;
     }
 
 }
