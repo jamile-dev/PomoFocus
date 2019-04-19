80db6361dff692562ef155ec74c171488022b53d
Jamile Lima <jamilelima@protonmail.com>
2019-04-19 17:10:25 -0300

    chore: refactor TasksDoneFragment to kotlin

---
+++ app/src/main/java/io/github/jamilelima/pomofocus/fragments/TasksDoneFragment.kt
@@ -0,0 +1,41 @@
+package io.github.jamilelima.pomofocus.fragments
+
+
+import android.arch.persistence.room.Room
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.github.jamilelima.pomofocus.adapters.TasksDoneAdapter
+import io.github.jamilelima.pomofocus.AppDatabase
+import io.github.jamilelima.pomofocus.R
+
+
+class TasksDoneFragment : Fragment() {
+
+    private var tasksDoneRecyclerView: RecyclerView? = null
+    private var tasksDoneAdapter: TasksDoneAdapter? = null
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val rootView = inflater.inflate(R.layout.fragment_tasks_done, container, false)
+
+        val db = Room
+                .databaseBuilder(activity!!.applicationContext, AppDatabase::class.java, "production").allowMainThreadQueries().build()
+
+        val tasksList = db.taskDao().doneTasks
+
+        tasksDoneRecyclerView = rootView.findViewById(R.id.task_done_recyclerview)
+        tasksDoneAdapter = TasksDoneAdapter(activity!!, tasksList)
+        tasksDoneRecyclerView?.layoutManager = LinearLayoutManager(activity)
+        tasksDoneRecyclerView?.adapter = tasksDoneAdapter
+
+        return rootView
+    }
+
+}
