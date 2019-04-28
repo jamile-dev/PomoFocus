902c57d58243ee6154b7044565f6129e9b875fea
jamilelima <jamilelima@protonmail.com>
2019-04-28 09:46:12 -0300

    refactor: remove android native methods from contract and return type

--- app/src/main/java/io/github/jamilelima/pomofocus/scenes/main/Main.kt
+++ app/src/main/java/io/github/jamilelima/pomofocus/scenes/main/Main.kt
@@ -1,17 +1,14 @@
 package io.github.jamilelima.pomofocus.scenes.main
 
-import android.view.MenuItem
-
 class Main {
 
     interface View {
-        fun onOptionsItemSelected(item: MenuItem): Boolean
         fun displaySettingsActivity()
         fun displayHowItWorksActivity()
     }
 
     interface Presenter {
-        fun presentSelectedOption(selectedId: Int, settingsId: Int, howItWorksId: Int): Boolean
+        fun presentSelectedOption(selectedId: Int, settingsId: Int, howItWorksId: Int)
     }
 
 }
