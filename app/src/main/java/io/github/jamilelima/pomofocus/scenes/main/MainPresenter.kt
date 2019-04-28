f986effaff1bc4245579db898ce538f1dd15b45e
jamilelima <jamilelima@protonmail.com>
2019-04-28 09:56:32 -0300

    refactor: change if to when and remove return type

--- app/src/main/java/io/github/jamilelima/pomofocus/scenes/main/MainPresenter.kt
+++ app/src/main/java/io/github/jamilelima/pomofocus/scenes/main/MainPresenter.kt
@@ -2,10 +2,10 @@ package io.github.jamilelima.pomofocus.scenes.main
 
 class MainPresenter(val view: Main.View) : Main.Presenter {
 
-    override fun presentSelectedOption(selectedId: Int, howItWorksId: Int, settingsOptionsId: Int): Boolean {
-        if (selectedId == howItWorksId) view.displayHowItWorksActivity()
-
-        if (selectedId == settingsOptionsId) view.displaySettingsActivity()
-        return true
+    override fun presentSelectedOption(selectedId: Int, howItWorksId: Int, settingsOptionsId: Int) {
+        when (selectedId) {
+            howItWorksId -> view.displayHowItWorksActivity()
+            settingsOptionsId -> view.displaySettingsActivity()
+        }
     }
 }
